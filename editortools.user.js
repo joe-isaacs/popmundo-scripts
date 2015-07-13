eba27a4faae972b4118156bb0a3003c6ba7557ae
joe-isaacs <joe.isaacs.pm@gmail.com>
2015-07-13 16:41:34 +0100

    more fixes

--- editortools.user.js
+++ editortools.user.js
@@ -5,6 +5,7 @@
 // @description     Prevents loss of changes in It's Pop articles
 // @version         1.0
 // @include         http://*.popmundo.com/Special/ED/Popmundo.aspx/EditArticle/*
+// @require         https://greasyfork.org/scripts/10938-etfunctions/code/etFunctions.js
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // @grant           GM_info
 // ==/UserScript==
@@ -12,16 +13,14 @@
 //jQuery
 var jisQuery = jQuery.noConflict();
 
+var script = document.createElement( "script" );
+script.type = "text/javascript";
+script.src = "https://greasyfork.org/scripts/10938-etfunctions/code/etFunctions.js";
+jisQuery( "head" ).append( script );
+
 jisQuery( '#ctl00_cphLeftColumn_ctl00_txtTitle' ).attr("onchange","_etChanged = true;");
 jisQuery( '#ctl00_cphLeftColumn_ctl00_txtPreamble' ).attr("onchange","_etChanged = true;");
 jisQuery( '#ctl00_cphLeftColumn_ctl00_txtText' ).attr("onchange","_etChanged = true;");
 jisQuery( '#ctl00_cphLeftColumn_ctl00_btnSave' ).attr("onchange","_etChanged = false;");
-jisQuery( 'body' ).attr("onbeforeunload","return etBeforeUnload();");
 jisQuery( '#aspnetForm' ).attr("onsubmit","return etOnSubmit();");
 jisQuery( '#ctl00_cphLeftColumn_ctl00_txtPublishDate' ).attr("onfocus","etChangeDate('ctl00_cphLeftColumn_ctl00_txtPublishDate');");
-
-
-
-
-
-
