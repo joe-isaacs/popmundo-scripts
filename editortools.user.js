123a7290b0092cc146fce84e792eca7180c626ac
joe-isaacs <joe.isaacs.pm@gmail.com>
2015-07-13 14:13:33 +0100

    Editor Tools

---
+++ editortools.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name            Joe's EditorTools for Popmundo
+// @namespace       http://popmundo-diaries.com/
+// @Author          Joe Isaacs CharId #3248185 <joe.isaacs.pm@gmail.com>
+// @description     Prevents loss of changes in It's Pop articles
+// @version         1.0
+// @include         http://*.popmundo.com/Special/ED/Popmundo.aspx/EditArticle/*
+// @require         https://greasyfork.org/scripts/10153-sptranslations/code/spTranslations.js
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
+// @grant           GM_info
+// ==/UserScript==
+
+//jQuery
+var jisQuery = jQuery.noConflict();
+
+jisQuery( '#ctl00_cphLeftColumn_ctl00_txtTitle' ).attr("onchange","_etChanged = true;");
+jisQuery( '#ctl00_cphLeftColumn_ctl00_txtPreamble' ).attr("onchange","_etChanged = true;");
+jisQuery( '#ctl00_cphLeftColumn_ctl00_txtText' ).attr("onchange","_etChanged = true;");
+jisQuery( '#ctl00_cphLeftColumn_ctl00_btnSave' ).attr("onchange","_etChanged = false;");
+jisQuery( 'body' ).attr("onbeforeunload","return etBeforeUnload();");
+jisQuery( '#aspnetForm' ).attr("onsubmit","return etOnSubmit();");
+jisQuery( '#ctl00_cphLeftColumn_ctl00_txtPublishDate' ).attr("onfocus","return etOnSubmit();");
+
+
+
+
+
+
---
+++ etFunctions.js
@@ -0,0 +1,39 @@
+// Translation labels object
+var _etChanged = false;
+
+function etBeforeUnload(){
+    if( _etChanged === false ) return true;    
+    return confirm( "You have unsaved content. Are you sure you want to leave?" );
+}
+
+function etOnSubmit(){
+    if( _etChanged === false ) return true;
+    return confirm( "You have unsaved content. Are you sure you want to change the article state?" );
+}
+
+jisQuery( '#aspnetForm' ).attr("onsubmit","return (");
+
+
+// Return tomorrow's date at 11:00 time
+function etGetDate(){
+    
+    var currentDate = new Date();
+    currentDate = new Date( new Date().getTime() + 24 * 60 * 60 * 1000 );
+    
+    var year = currentDate.getFullYear();
+	var month = (currentDate.getMonth() + 1) < 10 ? '0' + ( currentDate.getMonth() + 1 ) : ( currentDate.getMonth() + 1 );
+	var day = currentDate.getDate() < 10 ? '0' + currentDate.getDate() : currentDate.getDate();
+    var hour = '11';
+    var minutes = '00';
+	
+	return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes;
+
+}
+
+// Change the date text value if it's empty
+function etChangeDate( objectId ){
+	var obj = document.getElementById( objectId );
+	if( obj.value.length >= 1 ) return;
+	obj.value = etGetDate(); 
+	obj.setAttribute( "value", etGetDate() );
+}
