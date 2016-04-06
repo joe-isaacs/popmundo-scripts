28f73f0cc26b3c4b53a1d6cdc2203547c17a2458
joe-isaacs <joe.isaacs.pm@gmail.com>
2016-04-06 15:09:23 +0100

    Get your items fixed to work in Firefox

--- getYourItems.user.js
+++ getYourItems.user.js
@@ -3,7 +3,7 @@
 // @namespace       http://popmundo-diaries.com/
 // @Author	        Joe Isaacs CharId #3248185 <joe.isaacs.pm@gmail.com>
 // @description     Allows you to filter items of a specific character ID In a locale. Any problems, please contact Joe Isaacs (http://www.popmundo.com/World/Popmundo.aspx/Character/3248185)
-// @version         1.0.0
+// @version         1.0.1
 // @include         http://*.popmundo.com/World/Popmundo.aspx/Locale/ItemsEquipment/*
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // @grant           GM_info
@@ -11,6 +11,8 @@
 
 //jQuery
 var jisQuery = jQuery.noConflict();
+
+var isChrome = /Chrome/.test(navigator.userAgent);
 
 function getFilterCharacterID() {
     var url = location.href;
@@ -28,14 +30,21 @@ function getFilterLocaleBaseURL() {
                 url.substring( idxIni, idxLength) + "#";
 }
 
+
+var jsButton = "";
+if ( isChrome ) {
+    jsButton =  "window.location.reload(); "
+                + "window.location.href = '" + getFilterLocaleBaseURL() + "' + document.getElementById( 'textFilterID' ).value";
+} else {
+    jsButton =  "window.location.reload( '" + getFilterLocaleBaseURL() + "' + document.getElementById( 'textFilterID' ).value ); "
+}
+                
 var addElement =    "<tr class=\"group\"><td id=\"ctl00_cphLeftColumn_ctl00_repItemGroups_ctl01_tdCheckboxFiller\"></td><td colspan=\"2\">Filter Items</td></tr>"
                     + "<tr class=\"even hoverable\"><td id=\"ctl00_cphLeftColumn_ctl00_repItemGroups_ctl01_tdCheckboxFiller\"></td>"
                     + "<td>"
                     + "<input type=\"number\" id=\"textFilterID\" name=\"textFilterID\" placeholder=\"Character ID\" value=\"" + getFilterCharacterID() + "\">"
                     + "&nbsp;&nbsp;<button type=\"button\" onclick=\""
-                        + "window.location.reload(); window.location.href = '"
-                        + getFilterLocaleBaseURL() + "' + "
-                        + "document.getElementById( 'textFilterID' ).value"
+                    + jsButton
                     + "\">Filter</button>"
                     + "</td></tr>";
 
